0e11d7e1ed10378dd14719173b7cbad36709b458
Keannu Bernasol <noehbernasol0@gmail.com>
2023-11-12 22:10:54 +0800

    Added history viewing for equipment and equipment instances in django admin

--- equipment_tracker/equipments/admin.py
+++ equipment_tracker/equipments/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 from .models import Equipment, EquipmentInstance
 
-admin.site.register(Equipment)
-admin.site.register(EquipmentInstance)
+admin.site.register(Equipment, SimpleHistoryAdmin)
+admin.site.register(EquipmentInstance, SimpleHistoryAdmin)
