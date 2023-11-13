d6efa307593dda6b1a9258bc1b1a72f061f947d1
Keannu Bernasol <noehbernasol0@gmail.com>
2023-11-13 18:02:05 +0800

    Improved admin panel for equipment and equipment groups

--- equipment_tracker/equipment_groups/admin.py
+++ equipment_tracker/equipment_groups/admin.py
@@ -5,5 +5,5 @@ from simple_history.admin import SimpleHistoryAdmin
 
 @admin.register(EquipmentGroup)
 class EquipmentGroupAdmin(SimpleHistoryAdmin):
-    readonly_fields = ('status',)
+    readonly_fields = ['status']
     list_display = ('name', 'status', 'date_added', 'last_updated')
--- equipment_tracker/equipments/admin.py
+++ equipment_tracker/equipments/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 from .models import Equipment, EquipmentInstance
 
-admin.site.register(Equipment, SimpleHistoryAdmin)
-admin.site.register(EquipmentInstance, SimpleHistoryAdmin)
+
+@admin.register(Equipment)
+class EquipmentAdmin(SimpleHistoryAdmin):
+    readonly_fields = ('date_added', 'last_updated')
+    list_display = ('name', 'date_added', 'last_updated')
+
+
+@admin.register(EquipmentInstance)
+class EquipmentInstanceAdmin(SimpleHistoryAdmin):
+    list_display = ('equipment', 'status', 'remarks',
+                    'date_added', 'last_updated')
