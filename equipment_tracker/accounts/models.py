079b88fe7f4cc0cd3963eeb9f05dd88773717ec9
Keannu Bernasol <noehbernasol0@gmail.com>
2023-11-19 19:06:03 +0800

    Pre-generate accounts as technicians by default

--- equipment_tracker/accounts/models.py
+++ equipment_tracker/accounts/models.py
@@ -59,7 +59,7 @@ def create_superuser(sender, **kwargs):
         if not User.objects.filter(username=username).exists():
             # Create the superuser with is_active set to False
             superuser = User.objects.create_superuser(
-                username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+                username=username, email=email, password=password, first_name=first_name, last_name=last_name, is_technician=True)
 
             # Activate the superuser
             superuser.is_active = True
