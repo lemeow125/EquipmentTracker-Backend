c1ca024b4ee8310d11c213d81e566c13f3cb4f27
Keannu Bernasol <noehbernasol0@gmail.com>
2023-12-03 12:20:56 +0800

    Add is_technician field to django admin panel

--- equipment_tracker/accounts/admin.py
+++ equipment_tracker/accounts/admin.py
@@ -7,6 +7,9 @@ from .models import CustomUser
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = UserAdmin.list_display + ('is_technician',)
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_technician',)}),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
